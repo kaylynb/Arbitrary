0efcfadaeb8ec94db0aa572a36b8b0aa5b631b5e
Kaylyn Bogle <code@kaylynbogle.com>
2012-05-19 17:25:51 -0700

    Added constructor injection

--- Arbitrary/ArbitraryContainer.cs
+++ Arbitrary/ArbitraryContainer.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Reflection;
 
 namespace Arbitrary
@@ -12,30 +10,40 @@ namespace Arbitrary
         public ArbitraryContainer Register<TInterface, TInstance>(string key = null)
             where TInstance : TInterface
         {
-            _types[Tuple.Create<Type, string>(typeof(TInterface), key)] = typeof(TInstance);
+            _types[Tuple.Create(typeof(TInterface), key)] = typeof(TInstance);
             return this;
         }
 
         public T Resolve<T>(string key = null)
         {
-            Type t;
-            if (!_types.TryGetValue(Tuple.Create<Type, string>(typeof(T), key), out t))
-                return (T)Create(typeof(T));
+            return (T) Resolve(typeof (T), key);
+        }
 
-            return (T)Create(t);
+        public object Resolve(Type requestedType, string key = null)
+        {
+            Type t;
+            if (!_types.TryGetValue(Tuple.Create(requestedType, key), out t))
+                return Create(requestedType);
+
+            return Create(t);
         }
 
         private readonly Dictionary<Tuple<Type, string>, Type> _types = new Dictionary<Tuple<Type, string>, Type>();
 
         private object Create(Type type)
         {
-            var constructor = type.GetTypeInfo().DeclaredConstructors.FirstOrDefault();
+            var constructor = type.GetTypeInfo()
+                .DeclaredConstructors
+                .OrderByDescending(x => x.GetParameters().Length)
+                .FirstOrDefault();
+
             if (constructor == null)
                 throw new NoConstructorException("No constructor for type: " + type.FullName);
 
-            var parameters = new List<object>();
-
-            return constructor.Invoke(parameters.ToArray());
+            return constructor.Invoke(
+                constructor.GetParameters()
+                .Select(parameterInfo => Resolve(parameterInfo.ParameterType))
+                .ToArray());
         }
     }
 }
